0985b2e7aef88094083fc6c1867e1f3484b5b4c0
Amanda Cardoso <amandacarolinacardoso02@gmail.com>
2015-07-20 17:56:41 -0300

    Meu primeiro commit

---
+++ app/src/main/java/com/example/cardoso/projeto1/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.cardoso.projeto1;
+
+import android.content.ComponentCallbacks;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private static String TAG = MainActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView minhaView = (TextView) findViewById(R.id.textView);
+        minhaView.setText("Olá Android");
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"Cliquei!!",Toast.LENGTH_LONG).show();
+            }
+        });
+
+
+    }
+
+}
